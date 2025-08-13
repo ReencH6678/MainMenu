357941f04ffb6ecb9504587e62799960217d5164
ReencH6678 <ddaammiirr6678@gmail.com>
2025-08-13 15:53:45 +0200

    .

--- Assets/Scripst/AudioPlayer.cs
+++ Assets/Scripst/AudioPlayer.cs
@@ -6,11 +6,11 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioSource _audioSourse;
     private Button _playButton;
 
-    private void Awake
-        ()
+    private void Awake()
     {
         _playButton.onClick.AddListener(Play);
     }
+
     private void Play()
     {
         _audioSourse.Play();
